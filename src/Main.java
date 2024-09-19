0b2761928bd6856632a8ce7498cb880313622c47
27Dejwid <dawidpiejjo@gmail.com>
2024-09-19 10:28:20 +0200

    w

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Osoba {
+    public String imie;
+    public  String nazwisko;
+
+    public Osoba(String imie, String nazwisko, int wiek) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+
+    public int wiek;
+
+
+
+
+
+}
